7a744f0ad5727b4c32cd68d855ecec84d7c1ad65
Safdar Ali Shah <programmersafdar@live.com>
2024-08-27 12:04:04 +0500

    Setup Project reading for coding

---
+++ code/examples.ts
@@ -0,0 +1,21 @@
+// Type Annotations
+let num: number = 2343;
+num = 3;
+
+// Inferred Types
+
+// Union Types
+
+// Objects and Interfaces
+
+// Optional Properties
+
+// Classes
+
+// Generics
+
+// any, unknown, never
+
+// Type Assertions
+
+// @ts-ignore / @ts-nocheck
